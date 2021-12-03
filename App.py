b4f937986b7fb8ee56c5525da5fd29c82b09743e
Isaiah Magana <Maganaisaiah4@gmail.com>
2021-12-03 16:38:39 -0500

    Commented out the options in convert that arnt setup yet

--- App.py
+++ App.py
@@ -10,21 +10,21 @@ root = Tk()
 def convert():
     formula = looking_for.get()
     formula.lower()
-    
+
     if formula == 'find delta t':
         vi = float(initial_velocity.get())
         vf = float(final_velocity.get())
         acceleration = float(acceleration.get())
         change_in_time = round(find_change_in_time(vf, vi, acceleration), 2)
         messagebox.showinfo( "Change In Time", change_in_time)
-    elif vi == True and a == True and delta_t == True:
-        vi = float(input("What is your intital velocity: "))
-        acceleration = float(input("What is your acceleration: "))
-        change_in_time = float(input("What is your change in time: "))
-        final_velocity = round(find_final_velocity(vi, acceleration, change_in_time), 2)
-        print(f"The final velocity is {final_velocity}")
-    else:
-        print("Thanks anyway")
+    # elif vi == True and a == True and delta_t == True:
+    #     vi = float(input("What is your intital velocity: "))
+    #     acceleration = float(input("What is your acceleration: "))
+    #     change_in_time = float(input("What is your change in time: "))
+    #     final_velocity = round(find_final_velocity(vi, acceleration, change_in_time), 2)
+    #     print(f"The final velocity is {final_velocity}")
+    # else:
+    #     print("Thanks anyway")
 
 
 #Labels
